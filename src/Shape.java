e7b370b42a020762f551b4fcde23ab680034651d
OrukCagatay <cagatayoruk@posta.mu.edu.tr>
2025-04-25 11:56:27 +0300

    week-9

--- src/Shape.java
+++ src/Shape.java
@@ -1,28 +1,18 @@
-public class Shape {
+public abstract class Shape{
 
     private String color = "white";
 
-    public Shape(){
+    public  Shape() {
 
     }
-    public Shape(String color) {
+
+    public Shape(String color){
         this.color = color;
+        return;
     }
+    public abstract double perimeter();
 
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public double area(){
-        return 0;
-    }
-    public double perimeter(){
-        return 0;
-    }
+    public abstract double area();
 
 
     @Override
